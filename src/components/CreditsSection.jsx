4b7096715275346bfe60f6feb367cb9f0627a5fe
Majid Kargar <majidkargar01@gmail.com>
2023-09-09 15:26:12 +0430

    feat(component/Credits): Add Credits section component.

---
+++ src/components/CreditsSection.jsx
@@ -0,0 +1,14 @@
+const CreditsSection = () => {
+	return (
+		<section id="silverBox-CreditsSection" className="hidden">
+			<div className="container">
+				<h2 className="silverBox-sectionHeader">Credits</h2>
+
+				{/* <!-- team members --> */}
+				<div className="silverBox-teamMembers"></div>
+			</div>
+		</section>
+	);
+};
+
+export default CreditsSection;
