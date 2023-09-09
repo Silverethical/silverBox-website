1641afe2540c7697409926ac0ca090ec06ba5d69
Majid Kargar <majidkargar01@gmail.com>
2023-09-09 18:48:08 +0430

    feat(Header): Add smooth scrolling functionality
    Updated the Header component to enable smooth scrolling when clicking on links. Added a useEffect hook to handle smooth scrolling on page load and when the hash changes. Modified the handleLinkClick function to scroll to the corresponding section when inside the Documentation page. Used the useNavigate hook from react-router-dom for navigation in React Router v6.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
 	const [scrolled, setScrolled] = useState(false);
@@ -12,27 +13,105 @@ const Header = () => {
 		else setScrolled(false);
 	});
 
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const handleSmoothScrolling = () => {
+			const hash = window.location.hash;
+			if (hash) {
+				const targetElement = document.querySelector(hash);
+				if (targetElement) {
+					targetElement.scrollIntoView({
+						behavior: "smooth",
+						block: "start",
+					});
+				}
+			}
+		};
+
+		handleSmoothScrolling(); // Call it initially to handle scrolling on page load
+
+		window.addEventListener("hashchange", handleSmoothScrolling);
+
+		return () => {
+			window.removeEventListener("hashchange", handleSmoothScrolling);
+		};
+	}, []);
+
+	const handleLinkClick = (hash) => {
+		const targetElement = document.querySelector(hash);
+		if (targetElement) {
+			if (window.location.pathname === "/landing") {
+				targetElement.scrollIntoView({
+					behavior: "smooth",
+					block: "start",
+				});
+			} else {
+				navigate(`/landing${hash}`);
+				setTimeout(() => {
+					targetElement.scrollIntoView({
+						behavior: "smooth",
+						block: "start",
+					});
+				}, 0);
+			}
+		}
+	};
+
 	return (
-		<header id="silverBox-header" className={`${scrolled ? "scrolled" : ""} `}>
-			
+		<header
+			id="silverBox-header"
+			className={`${scrolled ? "scrolled" : ""} `}
+		>
 			<div className="container">
 				<nav>
-					<a href="/" id="logo">
-						<img src="/src/assets/images/silverBox-TransParent.svg" />
+					<Link to="/" id="logo">
+						<img
+							src="/src/assets/images/silverBox-TransParent.svg"
+							alt="SilverBox Logo"
+						/>
 						<span>SilverBox</span>
-					</a>
+					</Link>
 					<ul>
 						<li>
-							<a href="#silverBox-heroSection">Intro</a>
+							<Link
+								to="/landing"
+								onClick={() =>
+									handleLinkClick("#silverBox-heroSection")
+								}
+							>
+								Intro
+							</Link>
 						</li>
 						<li>
-							<a href="#silverBox-exampleSection">Example</a>
+							<Link
+								to="/landing"
+								onClick={() =>
+									handleLinkClick("#silverBox-exampleSection")
+								}
+							>
+								Example
+							</Link>
 						</li>
 						<li>
-							<a href="#silverBox-usageSection">Usage</a>
+							<Link
+								to="/landing"
+								onClick={() =>
+									handleLinkClick("#silverBox-usageSection")
+								}
+							>
+								Usage
+							</Link>
 						</li>
 						<li className="silverBox-documentation-header">
-							<a href="/documentation?v=latest">Documentation</a>
+							<Link
+								to="/documentation?v=latest"
+								onClick={() =>
+									handleLinkClick("#silverBox-header")
+								}
+							>
+								Documentation
+							</Link>
 						</li>
 					</ul>
 					<div id="hamburger-menu">
