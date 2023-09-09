8b0d9d20352a2ec1317f0b012a45f332426eb451
dofxo <kargat504@gmail.com>
2023-09-10 03:19:38 +0330

    feat(Example): add Example component

---
+++ src/pages/Home/Example.jsx
@@ -0,0 +1,75 @@
+import { useRef } from "react";
+import copyConfig from "../../helpers/copyConfig";
+import customStringify from "../../helpers/customStringify";
+
+const Example = ({ data }) => {
+	const iconRef = useRef(null);
+	const { explanation, config } = data;
+
+
+	const handleCopyExample = () => {
+		// calls the copyConfig
+		copyConfig({
+			copy: customStringify(config),
+			iconElement: iconRef.current,
+			timeOut: 1500,
+		});
+	};
+
+	const handleCopyButton = () => {
+		copyConfig({ copy: config });
+	};
+
+	return (
+		<div className="silverBox-example">
+			{/* First Example Column */}
+			<div className="silverBox-exampleColumn">
+				{/* Example explanation div */}
+				<div className="silverBox-exampleExplanation">
+					{explanation}
+				</div>
+				{/* Example button wrapper */}
+				<div className="silverBox-example-button-wrapper">
+					{/* Show button */}
+					<button
+						className="silverBox-showExample silverBox-exampleButtons"
+						onClick={() => {
+							silverBox(config);
+						}}
+					>
+						Show Me
+					</button>
+					{/* Copy config button */}
+					<button
+						className="silverBox-copyConfig silverBox-exampleButtons"
+						onClick={handleCopyButton}
+					>
+						Copy Code
+					</button>
+				</div>
+			</div>
+			{/* Second Example Column */}
+			<div className="silverBox-exampleColumn silverBox-exampleColumn-code">
+				{/* Example config */}
+				<div className="silverBox-exampleConfig">
+					<pre>
+						<code>{customStringify(config)}</code>
+					</pre>
+					{/* Copy example button */}
+					<button
+						className="silverBox-exampleCopyButton"
+						onClick={handleCopyExample}
+					>
+						<img
+							src="/src/assets/images/copyIcon.png"
+							alt="Copy"
+							ref={iconRef}
+						/>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Example
