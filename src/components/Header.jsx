20c37830c445541e288baf2415e15960f7596938
Majid Kargar <majidkargar01@gmail.com>
2023-09-09 15:23:01 +0430

    feat(component/Header): Add Header section component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+const Header = () => {
+	return (
+		<header id="silverBox-header">
+			<div className="container">
+				<nav>
+					<a href="/" id="logo">
+						<img src="/public/src/images/silverBox-TransParent.svg" />
+						<span>SilverBox</span>
+					</a>
+					<ul>
+						<li>
+							<a href="#silverBox-heroSection">Intro</a>
+						</li>
+						<li>
+							<a href="#silverBox-exampleSection">Example</a>
+						</li>
+						<li>
+							<a href="#silverBox-usageSection">Usage</a>
+						</li>
+						<li className="silverBox-documentation-header">
+							<a href="/documentation?v=latest">Documentation</a>
+						</li>
+					</ul>
+					<div id="hamburger-menu">
+						<div className="row"></div>
+						<div className="row"></div>
+						<div className="row"></div>
+					</div>
+				</nav>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
