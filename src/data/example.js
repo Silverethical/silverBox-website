e14f5f2561b6cc11dabaf5814ecaa3fb828eb4a8
dofxo <kargat504@gmail.com>
2023-09-09 19:41:30 +0330

    feat(example): add example rendering

---
+++ src/data/example.js
@@ -0,0 +1,89 @@
+const examples = [
+	{
+		explanation: "Simple success alert.",
+		config: {
+			title: {
+				text: "Success",
+				alertIcon: "success",
+			},
+			text: "Your task has been completed.",
+		},
+	},
+	{
+		explanation: "Error modal with a button.",
+		config: {
+			alertIcon: "error",
+			text: "Operation failed.",
+			centerContent: true,
+			cancelButton: {
+				text: "OK",
+			},
+		},
+	},
+	{
+		explanation: "Small informative message on top-right of the screen.",
+		config: {
+			position: "top-right",
+			alertIcon: "info",
+			text: "changes has been saved!",
+			centerContent: true,
+			showCloseButton: true,
+		},
+	},
+	{
+		explanation: "Auto closing pop-up message with a timer.",
+		config: {
+			timer: 2000,
+			customIcon: "src/assets/images/lightTimeout.png",
+			title: {
+				text: "You are so cute",
+			},
+			centerContent: true,
+		},
+	},
+	{
+		explanation: "A dark theme pop-up message.",
+		config: {
+			alertIcon: "info",
+			theme: "dark",
+			text: "Welcome to the dark side",
+			centerContent: true,
+			showCloseButton: true,
+		},
+	},
+	{
+		explanation:
+			"A login modal with a custom icon, a footer, and 2 different types of inputs.",
+		config: {
+			customIcon: "src/assets/images/loginExample.png",
+			title: {
+				text: "Login",
+			},
+			centerContent: true,
+			text: "Enter your account information",
+			footer: "<a href='#'>Forgot your password?</a>",
+			showCloseButton: true,
+			confirmButton: {
+				text: "Login",
+				closeOnClick: false,
+			},
+			cancelButton: {},
+			input: [
+				{
+					label: "Username",
+					type: "text",
+					placeHolder: "Enter your username",
+					maxLength: 30,
+				},
+				{
+					label: "Password",
+					type: "password",
+					placeHolder: "Enter your password",
+					hint: "Pick a strong password.",
+				},
+			],
+		},
+	},
+];
+
+export default examples;
--- src/pages/Home/ExampleSection.jsx
+++ src/pages/Home/ExampleSection.jsx
@@ -1,8 +1,82 @@
+import { useRef } from "react";
+import copyConfig from "../../helpers/copyConfig";
+import customStringify from "../../helpers/customStringify"
+import examples from "../../data/example";
+
+const ExampleComponent = ({ data }) => {
+	const iconRef = useRef(null);
+	const { explanation, config } = data;
+
+
+	const handleCopyExample = () => {
+		// calls the copyConfig
+		copyConfig({
+			copy: customStringify(config),
+			iconElement: iconRef.current,
+			timeOut: 1500,
+		});
+	};
+
+	const handleCopyButton = () => {
+		copyConfig({ copy: config });
+	};
+
+	return (
+		<div className="silverBox-example">
+			{/* First Example Column */}
+			<div className="silverBox-exampleColumn">
+				{/* Example explanation div */}
+				<div className="silverBox-exampleExplanation">
+					{explanation}
+				</div>
+				{/* Example button wrapper */}
+				<div className="silverBox-example-button-wrapper">
+					{/* Show button */}
+					<button className="silverBox-showExample silverBox-exampleButtons" 
+					onClick={()=>{
+						silverBox(config)
+					}}>
+						Show Me
+					</button>
+					{/* Copy config button */}
+					<button
+						className="silverBox-copyConfig silverBox-exampleButtons"
+						onClick={handleCopyButton}
+					>
+						Copy Code
+					</button>
+				</div>
+			</div>
+			{/* Second Example Column */}
+			<div className="silverBox-exampleColumn silverBox-exampleColumn-code">
+				{/* Example config */}
+				<div className="silverBox-exampleConfig">
+					<pre><code>{customStringify(config)}</code></pre>
+					{/* Copy example button */}
+					<button
+						className="silverBox-exampleCopyButton"
+						onClick={handleCopyExample}
+					>
+						<img
+							src="/src/assets/images/copyIcon.png"
+							alt="Copy"
+							ref={iconRef}
+						/>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
 const ExampleSection = () => {
 	return (
 		<section id="silverBox-exampleSection">
 			<div className="container">
 				<h2 className="silverBox-sectionHeader">Example</h2>
+				{examples.map((example, idx) => (
+					<ExampleComponent key={idx} data={example} />
+				))}
 			</div>
 		</section>
 	);
