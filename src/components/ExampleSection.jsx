db11b21e6e561c2a948145ed78ea3647db06180b
Majid Kargar <majidkargar01@gmail.com>
2023-09-09 15:22:22 +0430

    feat(components/Example): Add Example section component.

---
+++ src/components/ExampleSection.jsx
@@ -0,0 +1,11 @@
+const ExampleSection = () => {
+	return (
+		<section id="silverBox-exampleSection">
+			<div className="container">
+				<h2 className="silverBox-sectionHeader">Example</h2>
+			</div>
+		</section>
+	);
+};
+
+export default ExampleSection;
