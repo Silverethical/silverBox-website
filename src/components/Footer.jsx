7bcc081591d018d256727aa89077bd976b15a920
Majid Kargar <majidkargar01@gmail.com>
2023-09-09 15:22:48 +0430

    feat(component/Footer): Add Footer section component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+	return (
+		<footer>
+			<br />
+			Special thanks to{" "}
+			<a
+				href="https://sweetalert2.github.io/"
+				target="_blank"
+				id="sweetAlert"
+				rel="noreferrer"
+			>
+				SweetAlert2{" "}
+			</a>
+			and{" "}
+			<a
+				href="https://www.untitledui.com/"
+				target="_blank"
+				id="Untitled-UI"
+				rel="noreferrer"
+			>
+				Untitled UI
+			</a>
+		</footer>
+	);
+};
+
+export default Footer;
