d601fb0db2ffc4d38ebd76614fcaddac93c4ef60
Majid Kargar <majidkargar01@gmail.com>
2023-09-10 13:31:52 +0430

    refactor(formating): format App.jsx and main.jsx files.

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ const App = () => {
 	useEffect(() => {
         hljs.highlightAll();
     }, []);
+
 	return (
 		<>
 			<Header />
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./scss/index.scss";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
-import Documentation from "./pages/Documentation/Documentation.jsx"
+import Documentation from "./pages/Documentation/Documentation.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 			<Routes>
 				<Route path="/" element={<Navigate to="/landing" />} />
 				<Route path="/landing" element={<App />} />
-        <Route path="/documentation" element={<Documentation/>}/>
+				<Route path="/documentation" element={<Documentation />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
