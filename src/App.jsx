303a950111353c622089a76222353b2465d1f80f
Majid Kargar <majidkargar01@gmail.com>
2023-09-12 20:43:20 +0430

    refactor(Pages): Change Home directory to landing
    Also rename paths from Home to landing

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,20 @@ import { useEffect } from "react";
 
 // Components Import
 import Header from "./components/Header/Header";
-import HeroSection from "./pages/Home/HeroSection";
-import ExampleSection from "./pages/Home/Example/ExampleSection";
-import UsageSection from "./pages/Home/UsageSection";
-import CreditsSection from "./pages/Home/CreditsSection";
+import HeroSection from "./pages/Landing/HeroSection";
+import ExampleSection from "./pages/Landing/Example/ExampleSection";
+import UsageSection from "./pages/Landing/UsageSection";
+import CreditsSection from "./pages/Landing/CreditsSection";
 import Footer from "./components/Footer";
 
 // highlightJs imports
 import hljs from "highlight.js";
 import "./scss/libraries/_atom-one-dark-reasonable.scss";
 
-
 const App = () => {
-
 	useEffect(() => {
-        hljs.highlightAll();
-    }, []);
+		hljs.highlightAll();
+	}, []);
 
 	return (
 		<>
