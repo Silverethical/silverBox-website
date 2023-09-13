c77dd2f179560b8e896be6cb5172a9717f95ac42
dofxo <kargat504@gmail.com>
2023-09-13 22:24:25 +0330

    refactor(Header): change header Logo navigation path

--- src/components/Header/SilverBoxLogo.jsx
+++ src/components/Header/SilverBoxLogo.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import basePath from "../../constants/basePath.js";
 
 const SilverBoxLogo = ({ silverBoxLogoRef }) => {
 	return (
-		<Link to="/" id="logo" ref={silverBoxLogoRef}>
+		<Link to={`${basePath}/`} id="logo" ref={silverBoxLogoRef}>
 			<img
 				src="./images/silverBox-TransParent.png"
 				alt="SilverBox Logo"
