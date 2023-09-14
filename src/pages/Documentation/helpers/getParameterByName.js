e103219b4c22050e38870273550635c745583dc2
Majid Kargar <majidkargar01@gmail.com>
2023-09-14 08:21:48 +0430

    feat(documentation/helpers): Add getParameterByName.js file.

---
+++ src/pages/Documentation/helpers/getParameterByName.js
@@ -0,0 +1,10 @@
+// Helper function to get query string value from URL
+const getParameterByName = (name, url = window.location.href) => {
+    name = name.replace(/[[\]]/g, "\\$&");
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    const results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+export default getParameterByName;
