f00a05140babbffcaa26fc61f6ec5cedd1221b8e
Majid Kargar <majidkargar01@gmail.com>
2023-09-14 08:30:32 +0430

    feat(Documentation): Add Documentation component.

--- src/pages/Documentation/Documentation.jsx
+++ src/pages/Documentation/Documentation.jsx
@@ -1,75 +1,31 @@
-import { useEffect, useState } from "react";
-import SilverBoxDocumentationTableComponent from "./helpers/documentationTableComponent";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer";
-import documentations from "./data/documentations/index";
-import getParameterByName from "./helpers/getParameterByName";
-
-const Documentation = () => {
-    const [versionNumber, setVersionNumber] = useState("");
-
-    useEffect(() => {
-        // All versions of documentation
-        const allVersions = ["1.0.0", "1.1.0", "1.2.0"];
-
-        // Version number from query string URL
-        let queryStringVersionNumber = getParameterByName("v");
-
-        // If version number from query string is not given or does not exist in allVersions array
-        if (
-            !queryStringVersionNumber ||
-            !allVersions.includes(queryStringVersionNumber)
-        ) {
-            // Set the versionNumber to the latest version
-            queryStringVersionNumber = allVersions[allVersions.length - 1];
-        }
-
-        setVersionNumber(queryStringVersionNumber);
-    }, []);
-
-    useEffect(() => {
-        // Render the SilverBoxDocumentationTableComponent once the versionNumber is set
-        if (versionNumber) {
-            const docs =
-                documentations[`v${versionNumber.replaceAll(".", "_")}`];
-            SilverBoxDocumentationTableComponent(
-                ".silverBox-tableWrapper",
-                docs
-            );
-        }
-    }, [versionNumber]);
-
+import propTypes from "prop-types";
+const Documentation = ({ versionNumber }) => {
     return (
-        <>
-            {/* Header component */}
-            <Header />
-
-            {/* The main section */}
-            <main>
-                <section id="silverBox-documentationSection">
-                    <div className="container">
-                        <div className="silverBox-tableWrapper">
-                            <div>
-                                <h2 className="silverBox-sectionHeader">
-                                    <span id="silverBox-documentation">
-                                        Documentation{" "}
-                                        <span id="silverBox-documentation-version">
-                                            {versionNumber
-                                                ? `v${versionNumber}`
-                                                : "Loading version..."}
-                                        </span>
+        <main>
+            <section id="silverBox-documentationSection">
+                <div className="container">
+                    <div className="silverBox-tableWrapper">
+                        <div>
+                            <h2 className="silverBox-sectionHeader">
+                                <span id="silverBox-documentation">
+                                    Documentation{" "}
+                                    <span id="silverBox-documentation-version">
+                                        {versionNumber
+                                            ? `v${versionNumber}`
+                                            : "Loading version..."}
                                     </span>
-                                </h2>
-                            </div>
+                                </span>
+                            </h2>
                         </div>
                     </div>
-                </section>
-            </main>
-
-            {/* Footer Component */}
-            <Footer />
-        </>
+                </div>
+            </section>
+        </main>
     );
+};
+
+Documentation.propTypes = {
+    versionNumber: propTypes.string,
 };
 
 export default Documentation;
